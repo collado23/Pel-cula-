de6234ac19108afdce547afea9ae1d6524e9b35d
collado23 <collado23aaa@gmail.com>
2026-07-15 21:38:02 -0300

    Refactor Canales.js to include GestorCanales class

--- Canales.js
+++ Canales.js
@@ -1,33 +1,36 @@
-var LISTA_CANALES = `#EXTM3U
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_america-tv_m.png" group-title="Argentina",America TV
-https://stream-gtlc.telecentro.net.ar/hls/americatvHD/americatvHD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_telefe-hd_m.png" group-title="Argentina",Telefe HD
-https://stream-gtlc.telecentro.net.ar/hls/telefeHD/telefeHD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_el-trece-hd_m.png" group-title="Argentina",El Trece HD
-https://stream-gtlc.telecentro.net.ar/hls/el13HD/el13HD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_el-nueve_m.png" group-title="Argentina",El Nueve
-https://stream-gtlc.telecentro.net.ar/hls/elnueveHD/elnueveHD.m3u8
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Canal_7_Argentina_logo.svg/200px-Canal_7_Argentina_logo.svg.png" group-title="Argentina",Canal 7 Publica
-https://stream-gtlc.telecentro.net.ar/hls/canal7HD/canal7HD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_cronica-tv_m.png" group-title="Noticias",Cronica TV
-https://stream-gtlc.telecentro.net.ar/hls/cronicatvHD/cronicatvHD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_canal-26_m.png" group-title="Noticias",Canal 26
-https://d1gymyavdvyjgt.cloudfront.net/v1/manifest/3722c60a815c199d9c1ef46cf4e8a03207b528c4/canal26/PROD/index.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_a24_m.png" group-title="Noticias",A24
-https://stream-gtlc.telecentro.net.ar/hls/a24HD/a24HD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_tyc-sports_m.png" group-title="Deportes",TyC Sports
-https://stream-gtlc.telecentro.net.ar/hls/tycsportsHD/tycsportsHD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_net-tv_m.png" group-title="Argentina",Net TV
-https://stream-gtlc.telecentro.net.ar/hls/nettvHD/nettvHD.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_magazine_m.png" group-title="Entretenimiento",Magazine
-https://stream-gtlc.telecentro.net.ar/hls/magazineHD/magazineHD.m3u8
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Deportv_logo.svg/200px-Deportv_logo.svg.png" group-title="Deportes",DeporTV
-https://d1gymyavdvyjgt.cloudfront.net/v1/manifest/3722c60a815c199d9c1ef46cf4e8a03207b528c4/deportv/PROD/index.m3u8
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Pakapaka_logo.svg/200px-Pakapaka_logo.svg.png" group-title="Infantil",Pakapaka
-https://d1gymyavdvyjgt.cloudfront.net/v1/manifest/3722c60a815c199d9c1ef46cf4e8a03207b528c4/pakapaka/PROD/index.m3u8
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Encuentro_logo.svg/200px-Encuentro_logo.svg.png" group-title="Documentales",Encuentro
-https://d1gymyavdvyjgt.cloudfront.net/v1/manifest/3722c60a815c199d9c1ef46cf4e8a03207b528c4/encuentro/PROD/index.m3u8
-#EXTINF:-1 tvg-logo="https://cdn.mitvstatic.com/channels/ar_ln_m.png" group-title="Noticias",LN+
-https://d1gymyavdvyjgt.cloudfront.net/v1/manifest/3722c60a815c199d9c1ef46cf4e8a03207b528c4/ln/PROD/index.m3u8
-#EXTINF:-1 tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Cablenoticias_logo.svg/200px-Cablenoticias_logo.svg.png"
+class GestorCanales {
+    constructor() {
+        this.canales = [];
+    }
 
+    parsearM3U(contenido) {
+        const lineas = contenido.split('\n');
+        let canalActual = {};
+        
+        lineas.forEach(linea => {
+            linea = linea.trim();
+            if (!linea) return;
+
+            if (linea.startsWith('#EXTINF:')) {
+                const nombre = linea.split(',').pop();
+                canalActual.nombre = nombre;
+            } else if (linea.startsWith('http') || linea.startsWith('rtmp')) {
+                canalActual.url = linea;
+                this.canales.push({...canalActual});
+                canalActual = {};
+            }
+        });
+        return this.canales;
+    }
+
+    async cargarArchivo(ruta = 'canales.m3u') {
+        try {
+            const res = await fetch(ruta);
+            const texto = await res.text();
+            return this.parsearM3U(texto);
+        } catch (err) {
+            console.error('Error al cargar lista:', err);
+            return [];
+        }
+    }
+}
